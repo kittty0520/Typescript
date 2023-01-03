4cf3689a4782278a01b3104e5af2eb537788d838
kittty0520 <105909450+kittty0520@users.noreply.github.com>
2023-01-03 17:32:05 +0900

    update

--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,32 +1,37 @@
-interface Email {
-  value: string;
+interface DropdwonItem<T>{
+  value:T;
   selected: boolean;
 }
 
-const emails : Email[] = [
+// interface Email {
+//   value: string;
+//   selected: boolean;
+// }
+
+const emails : DropdwonItem<string>[] = [
   { value: 'naver.com', selected: true },
   { value: 'gmail.com', selected: false },
   { value: 'hanmail.net', selected: false },
 ];
 
-interface ProductNumber {
-  value: number;
-  selected: boolean;
-}
+// interface ProductNumber {
+//   value: number;
+//   selected: boolean;
+// }
 
-interface TrueFalse {
-  value: boolean;
-  selected: boolean;
-}
+// interface TrueFalse {
+//   value: boolean;
+//   selected: boolean;
+// }
 
 
-const numberOfProducts :ProductNumber[] = [
+const numberOfProducts :DropdwonItem<number>[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item :  Email | ProductNumber) {
+function createDropdownItem<T>(item : DropdwonItem<T>) {
   const option = document.createElement('option');
   option.value = item.value.toString();
   option.innerText = item.value.toString();
@@ -36,13 +41,13 @@ function createDropdownItem(item :  Email | ProductNumber) {
 
 // NOTE: 이메일 드롭 다운 아이템 추가
 emails.forEach(function (email) {
-  const item = createDropdownItem(email);
+  const item = createDropdownItem<string>(email);
   const selectTag = document.querySelector('#email-dropdown');
   selectTag.appendChild(item);
 });
 
 numberOfProducts.forEach(function (product){
-  const item = createDropdownItem(product);
+  const item = createDropdownItem<number>(product);
   const selectTag = document.querySelector('#product-dropdown');
   selectTag.appendChild(item);
 })
--- quiz/2_address-book/src/promise.ts
+++ quiz/2_address-book/src/promise.ts
@@ -4,3 +4,12 @@ function fetchItems() {
 }
 let result = fetchItems();
 console.log(result); //string 배열이 반환됨
+
+
+//함수를 실행하는 시점에서 promise내부의 비동기적 코드의 타입을 알 수 없다.(unknown)=>제네릭타입 사용해야 한다.
+function fetchItems():Promise<string[]>{
+    let items = ['a', 'b', 'c'];
+    return new Promise(function(resolve){
+        resolve(items);
+    });
+}
