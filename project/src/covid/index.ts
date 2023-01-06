079f8b8f200d3b4697821ade50f03bb4fa392394
kittty0520 <105909450+kittty0520@users.noreply.github.com>
2023-01-06 18:56:39 +0900

    Define Type of API Functions

---
+++ project/src/covid/index.ts
@@ -0,0 +1,46 @@
+export interface Country {
+  Country: string;
+  CountryCode: string;
+  Date: string;
+  ID: string;
+  NewConfirmed: number;
+  NewDeaths: number;
+  NewRecovered: number;
+  Premium: any;
+  Slug: string;
+  TotalConfirmed: number;
+  TotalDeaths: number;
+  TotalRecovered: number;
+}
+
+interface Global {
+  Date: string;
+  NewConfirmed: number;
+  NewDeaths: number;
+  NewRecovered: number;
+  TotalConfirmed: number;
+  TotalDeaths: number;
+  TotalRecovered: number;
+}
+export interface CovidSummaryResponse {
+  Countries: Country[];
+  Date: string;
+  Global: Global;
+  Message: string;
+}
+
+export interface CountrySummaryInfo {
+  Cases: number;
+  City: string;
+  CityCode: string;
+  Country: string;
+  CountryCode: string;
+  Date: string;
+  Lat: string;
+  Lon: string;
+  Province: string;
+  Status: string;
+}
+
+//데이터가 배열 내부로 받아오기때문에 interface로 바로 쓸 수 없다.
+export type CountrySummaryResponse = CountrySummaryInfo[];
